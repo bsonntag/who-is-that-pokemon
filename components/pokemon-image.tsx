e05cf21d6e9c23346bae09f211d62f4cad6c2bca
Benjamim Sonntag <benjamimsonntag@gmail.com>
2020-12-12 01:13:37 +0000

    Fix image entry animation

--- components/pokemon-image.tsx
+++ components/pokemon-image.tsx
@@ -1,5 +1,6 @@
 import classNames from 'clsx';
 import Image from 'next/image';
+import { useState } from 'react';
 import styles from './pokemon-image.module.css';
 
 type Props = {
@@ -8,13 +9,16 @@ type Props = {
 };
 
 export function PokemonImage({ isRevealed, url }: Props): JSX.Element {
+  const [loaded, setLoaded] = useState(false);
+
   return (
-    <div className={styles.wrapper}>
+    <div className={classNames(styles.wrapper, loaded && styles.appear)}>
       <Image
         className={classNames(styles.image, isRevealed && styles.revealed)}
         src={url}
         width={500}
         height={500}
+        onLoad={() => setLoaded(true)}
       />
     </div>
   );
