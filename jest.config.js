0189311e9eaa6a3a0229229ef622c862201fc894
Benjamim Sonntag <benjamimsonntag@gmail.com>
2020-12-13 02:02:04 +0000

    Fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
+const tsConfig = require('./tsconfig.json');
+
 module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['html', 'lcov', 'text'],
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        ...tsConfig.compilerOptions,
+        jsx: 'react',
+        target: 'es2016',
+      },
+    },
+  },
 };
